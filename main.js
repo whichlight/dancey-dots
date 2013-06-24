705921d14eb90c317a7d41bdb353bd8937f35539
Noura <culurienneldoreth@gmail.com>
2013-06-24 16:31:27 -0400

    refactored things into dots, where each dot handles drawing itself, prepping its synths, moving, and destroying itself

--- main.js
+++ main.js
@@ -1,3 +1,58 @@
+function dot(data, context) {
+    this.id = data.id;
+    this.col = data.col;
+
+    var _x, _y, oscillator, filter, gainNode;
+
+    var $el = $(
+        '<span class="synth" id="synth_'+data.id+'" style="background-color:'+this.col+';">'+
+            '<span class="chat" style="display:none;"/>'+
+        '</span>');
+    $('body').append($el);
+
+    function prep_synths() {
+        oscillator = context.createOscillator();
+        oscillator.type = 1;
+        filter = context.createBiquadFilter();
+        filter.type = 0;
+        gainNode = context.createGainNode();
+        gainNode.gain.value = 0.05;
+
+        oscillator.connect(filter);
+        filter.connect(gainNode);
+        gainNode.connect(context.destination);
+    }
+    prep_synths();
+
+    this.move = function(data) {
+        _x = 30 + Math.pow(2, data.x/72.0);
+        _y = 200 + Math.pow(2.5, ($(window).height() - data.y)/72.0);
+        oscillator.frequency.value = _x;
+        filter.frequency.value = _y;
+        oscillator.start(0);
+        $el.css({
+            left: (data.x - 20) + 'px',
+            top: (data.y - 20) + 'px',
+            opacity: 0.7,
+        });
+    };
+    this.move(data);
+
+    this.silent = function() {
+        oscillator.stop(0);
+        prep_synths();
+        $el.css('opacity', '0.2');
+    };
+
+    this.end = function() {
+        oscillator.stop(0);
+        $('body').remove($el);
+    };
+
+    return this;
+}
+
+
 $(document).ready(function() {
     try{
         var context = new webkitAudioContext(),
@@ -7,106 +62,54 @@ $(document).ready(function() {
         alert("this uses the web audio API, try opening it in google chrome \n\n <3 whichlight" );
     }
 
-    var synths = {};
-    var pressed;
-
-    //prepare the synths, fill buffer to play
-
-    //mouse down, play your synth
-    $(document).mousedown(function(e){
-        pressed = true;
-        sendMousePosition(e);
-    });
-
-    //mouse up, stop playing your synth, and prepare the buffer
-    $(document).mouseup(function(){
-        //oscillator.noteOff(0);
-        //prepSynths();
-        pressed = false;
-        socket.emit('silent',{state:"stop"});
-    });
+    var dots = {};
+    var pressed = false;
 
     //socketio
     //var socket = io.connect('http://whichlight.synth_fun.jit.su/');
     var socket = io.connect('http://localhost:8000');
 
-    function synthmap(x,y){
-        tx = 30+Math.pow(2,x/72.0);
-        ty = 200+Math.pow(2.5,($(window).height()-y)/72.0);
-        return [tx,ty]
-    }
-
-    document.onmousemove = sendMousePosition;
-
-    socket.on('silent',function(id){
-        $('#synth_'+id).css({'opacity' : '0.2'});
-        synths[id][0].noteOff(0);
-        synths[id] = prepSynths();
+    //sending events to the server
+    $(document).mousedown(function(e){
+        pressed = true;
+        sendMousePosition(e);
     });
 
-    socket.on('move', function (data) {
-        if($('#synth_'+data['id']).length == 0) {
-            $('body').append('<span class="synth" id="synth_'+data['id']+'"><span style="display:none;" class="chat"/></span>');
-        }
-
-        $('#synth_'+data['id']).css({
-            'left' : (data['x']-20) + 'px',
-            'top' : data['y']-20 + 'px',
-            'background-color': data['col'],
-            'opacity' : '0.7'
-        });
-
-        socket.on('close', function (id) {
-            $('#synth_'+id).remove();
-        });
-
-        if(!synths[data.id]){
-            synths[data.id] = prepSynths();
-        }
-        var n = synthmap(data.x, data.y);
-        _x = n[0];
-        _y = n[1];
-        if(synths[data.id]){
-            if(synths[data.id][0] && synths[data.id][1]){
-                synths[data.id][0].frequency.value = _x;
-                synths[data.id][1].frequency.value = _y;
-                synths[data.id][0].noteOn(0);
-            }
-        }
-    });
-
-    var sendMousePosition = _.throttle(function(mp){
+    function sendMousePosition(e) {
         if(pressed){
             socket.emit('move', {
-                x: mp.pageX,
-                y: mp.pageY,
+                x: e.pageX,
+                y: e.pageY,
                 w: $(window).width(),
                 h: $(window).height()
 			});
         }
-        return true;
-    }, 40);
+    }
 
-    function prepSynths(){
-        var oscillator = context.createOscillator(); //need perens here
-        oscillator.type = 1;
-        oscillator.frequency.value = _x;
+    document.onmousemove = _.throttle(sendMousePosition, 40);
 
-        //filter
-        var filter = context.createBiquadFilter();
-        filter.type = 0;
-        filter.frequency.value = _y;
+    $(document).mouseup(function(){
+        pressed = false;
+        socket.emit('silent', {
+            state:"stop"
+        });
+    });
 
-        //volume
-        var gainNode = context.createGainNode();
-        gainNode.gain.value = 0.05;
+    //responding to events from the server
+    socket.on('silent',function(id){
+        dots[id].silent();
+    });
 
-        //connect it all
-        oscillator.connect(filter);
-        filter.connect(gainNode);
-        gainNode.connect(context.destination);
+    socket.on('move', function (data) {
+        if (!dots[data.id]) {
+            dots[data.id] = dot(data, context);
+        }
+        dots[data.id].move(data);
+    });
 
-		return [oscillator, filter];
-	}
+    socket.on('close', function (id) {
+        dots[id].end();
+        delete dots[id];
+    });
 });
 
