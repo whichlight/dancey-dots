f612395edb89eae37136f8ba212e10fe057014ac
Noura <culurienneldoreth@gmail.com>
2013-06-24 14:34:52 -0400

    redid spacing in main.js

--- main.js
+++ main.js
@@ -1,135 +1,117 @@
-
-
-
 $(document).ready(function() {
-	try{
-	var context = new webkitAudioContext(),
-	_x, _y;
-	}
-	catch(err){
-	alert("this uses the web audio API, try opening it in google chrome \n\n <3 whichlight" );
+    try{
+        var context = new webkitAudioContext(),
+	    _x, _y;
+    }
+    catch(err){
+        alert("this uses the web audio API, try opening it in google chrome \n\n <3 whichlight" );
+    }
 
-	}
-	
-	var col;
+    var col;
+    var synths = {};
+    var pressed;
+    var time=(new Date()).getTime();
 
-	var synths = {};
-	var pressed;
-	var time=(new Date()).getTime();
+    //prepare the synths, fill buffer to play
 
-	//prepare the synths, fill buffer to play
+    //mouse down, play your synth
+    $(document).mousedown(function(e){	
+        pressed = true;
+        sendMousePosition(e);
+    });
 
+    //mouse up, stop playing your synth, and prepare the buffer
+    $(document).mouseup(function(){
+        //oscillator.noteOff(0);
+        //prepSynths();	
+        pressed=false;
+        socket.emit('silent',{state:"stop"});
+    });
 
-	//mouse down, play your synth
+    //socketio
+    //var socket = io.connect('http://whichlight.synth_fun.jit.su/');
+    var socket = io.connect('http://localhost:8000');
 
-	$(document).mousedown(function(e){	
-		
-		pressed= true;
-		sendMousePosition(e);
-		});
+    function synthmap(x,y){
+        tx = 30+Math.pow(2,x/72.0);
+        ty = 200+Math.pow(2.5,($(window).height()-y)/72.0);
+        return [tx,ty]
+    }
 
-	//mouse up, stop playing your synth, and prepare the buffer
-	$(document).mouseup(function(){
-		//	oscillator.noteOff(0);
-		//	prepSynths();	
-			pressed=false;
-			socket.emit('silent',{state:"stop"});
-		});
+    document.onmousemove = sendMousePosition;
 
+    socket.on('silent',function(id){
+        $('#synth_'+id).css({'opacity' : '0.2'});
+        synths[id][0].noteOff(0);
+        synths[id] = prepSynths();
+    });
 
-	//socketio
+    socket.on('move', function (data) {
+        if($('#synth_'+data['id']).length == 0) {
+            $('body').append('<span class="synth" id="synth_'+data['id']+'"><span style="display:none;" class="chat"/></span>');
+        }
 
+        $('#synth_'+data['id']).css({
+            'left' : (data['x']-20) + 'px',
+            'top' : data['y']-20 + 'px',
+            'background-color': data['col'],
+            'opacity' : '0.7'
+        });
 
-//	var socket = io.connect('http://whichlight.synth_fun.jit.su/');
-	var socket = io.connect('http://localhost:8000');
+        socket.on('close', function (id) {
+            $('#synth_'+id).remove();
+        });
 
-	function synthmap(x,y){
-		tx = 30+Math.pow(2,x/72.0);
-		ty = 200+Math.pow(2.5,($(window).height()-y)/72.0);
-		return [tx,ty]
-	}
-	
-	document.onmousemove = sendMousePosition;
+        if(!synths[data.id]){
+            synths[data.id]=prepSynths();
+        }
+        var n = synthmap(data.x,data.y);
+        _x = n[0];
+        _y = n[1];
+        if(synths[data.id]){
+            if(synths[data.id][0] && synths[data.id][1]){
+                synths[data.id][0].frequency.value = _x;
+                synths[data.id][1].frequency.value = _y;
+                synths[data.id][0].noteOn(0);
+            }
+        }
+    });
 
-	socket.on('silent',function(id){
-		$('#synth_'+id).css({
-		'opacity' : '0.2'
-      		});
-		synths[id][0].noteOff(0);
-		synths[id]=prepSynths(); 	
-	});
-
-	socket.on('move', function (data) {
- 		if($('#synth_'+data['id']).length == 0) {
-        $('body').append('<span class="synth" id="synth_'+data['id']+'"><span style="display:none;" class="chat"/></span>');
-	
-	}
-
-	$('#synth_'+data['id']).css({
-        'left' : (data['x']-20) + 'px',
-        'top' : data['y']-20 + 'px', 
-	'background-color': data['col'],
-	'opacity' : '0.7'
-      });
-      
-socket.on('close', function (id) {
-      $('#synth_'+id).remove();
-    });	
-
-		if(!synths[data.id]){
-		synths[data.id]=prepSynths(); 		 
-		}
-		var n = synthmap(data.x,data.y);  
-		 _x=n[0];
-		 _y=n[1];
-		if(synths[data.id]){		
-		if(synths[data.id][0] && synths[data.id][1]){	
-			synths[data.id][0].frequency.value=_x;
-			synths[data.id][1].frequency.value=_y;
-		
-			synths[data.id][0].noteOn(0);
-		}
-		
-		}
-
-    		});	
-	 
-	function sendMousePosition(mp) {
-		//if mouse is pressed, emit
-		var now = (new Date()).getTime();	
-		if(pressed && (now-time>40)){
-			socket.emit('move', {
-			  x: mp.pageX,
-			  y: mp.pageY,
-			  w: $(window).width(),
-			  h: $(window).height()
+    function sendMousePosition(mp) {
+        //if mouse is pressed, emit
+        var now = (new Date()).getTime();
+        if(pressed && (now-time>40)){
+            socket.emit('move', {
+                x: mp.pageX,
+                y: mp.pageY,
+                w: $(window).width(),
+                h: $(window).height()
 			});
-			time= (new Date()).getTime();
-		}
+            time= (new Date()).getTime();
+        }
+        return true;
+    }
 
-	  return true;	
+    function prepSynths(){
+        var oscillator = context.createOscillator(); //need perens here
+        oscillator.type=1;
+        oscillator.frequency.value=_x;
+
+        //filter
+        var filter = context.createBiquadFilter();
+        filter.type = 0;
+        filter.frequency.value = _y;
+
+        //volume
+        var gainNode = context.createGainNode();
+        gainNode.gain.value=0.05;
+
+        //connect it all
+        oscillator.connect(filter);
+        filter.connect(gainNode);
+        gainNode.connect(context.destination);
+
+		return [oscillator, filter];
 	}
-	   
-
-	function prepSynths(){
-		var oscillator = context.createOscillator(); //need perens here
-		oscillator.type=1;
-		oscillator.frequency.value=_x;
-		//filter 
-		var filter = context.createBiquadFilter();
-		filter.type=0;
-		filter.frequency.value=_y;
-
-		//volume
-		var gainNode = context.createGainNode();
-		gainNode.gain.value=0.05;
-			
-		//connect it all 
-		oscillator.connect(filter);
-		filter.connect(gainNode);
-		gainNode.connect(context.destination);
-
-		return [oscillator, filter]
-		}
 });
 
