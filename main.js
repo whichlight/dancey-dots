d391d453338f134f41eec5faf5889f2b9ff7e81f
Kawandeep Virdee <whichlight@gmail.com>
2014-05-21 01:52:46 -0400

    works on mobile, cleaned up a bit

--- main.js
+++ main.js
@@ -1,133 +1,226 @@
+var context;
+var col;
+var _x, _y;
+var synths = {};
+var pressed;
+var time=(new Date()).getTime();
+var client_id;
+
+
+var checkFeatureSupport = function(){
+  try{
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+  }
+  catch (err){
+    alert('web audio not supported');
+  }
+
+  if (!window.DeviceMotionEvent) {
+    alert("DeviveMotionEvent not supported");
+  }
+}
 
 
 
 $(document).ready(function() {
-	try{
-	var context = new webkitAudioContext(),
-	_x, _y;
-	}
-	catch(err){
-	alert("this uses the web audio API, try opening it in google chrome \n\n <3 whichlight" );
+  try{
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    var context = new AudioContext();
+  }
+  catch(err){
+    alert("this uses the web audio API, try opening it in google chrome \n\n <3 whichlight" );
+  }
 
-	}
+//  alert("Turn the volume up and touch the screen. Share the URL with a friend to hear their sounds. Let's have a wild synth party! \n\n <3 @whichlight");
 
-	var col;
+  var color = hsvToRgb(Math.random(),1,1);
+  col = 'rgb(' + color.join(',') + ')';
 
-	var synths = {};
-	var pressed;
-	var time=(new Date()).getTime();
-
-	//prepare the synths, fill buffer to play
-
-
-	//mouse down, play your synth
-
-	$(document).mousedown(function(e){
-
-		pressed= true;
-		sendMousePosition(e);
-		});
-
-	//mouse up, stop playing your synth, and prepare the buffer
-	$(document).mouseup(function(){
-		//	oscillator.noteOff(0);
-		//	prepSynths();
-			pressed=false;
-			socket.emit('silent',{state:"stop"});
-		});
-
-
-	//socketio
-
-    var socket = io.connect('http://'+window.location.hostname);
-
-	function synthmap(x,y){
-		tx = 30+Math.pow(2,x/72.0);
-		ty = 200+Math.pow(2.5,($(window).height()-y)/72.0);
-		return [tx,ty]
-	}
-
-	document.onmousemove = sendMousePosition;
-
-	socket.on('silent',function(id){
-		$('#synth_'+id).css({
-		'opacity' : '0.2'
-      		});
-		synths[id][0].noteOff(0);
-		synths[id]=prepSynths();
-	});
-
-	socket.on('move', function (data) {
- 		if($('#synth_'+data['id']).length == 0) {
-        $('body').append('<span class="synth" id="synth_'+data['id']+'"><span style="display:none;" class="chat"/></span>');
-
-	}
-
-	$('#synth_'+data['id']).css({
-        'left' : (data['x']-20) + 'px',
-        'top' : data['y']-20 + 'px',
-	'background-color': data['col'],
-	'opacity' : '0.7'
-      });
-
-socket.on('close', function (id) {
-      $('#synth_'+id).remove();
+    checkFeatureSupport();
+    $fun = document.getElementById("fun");
+    hammertime = Hammer($fun, {
+      prevent_default: true,
+      no_mouseevents: true
+    })
+    .on('touch', function(event){
+      touchActivate(event);
+    })
+    .on('drag', function(event){
+      touchActivate(event);
+    })
+    .on('release', function(event){
+      touchDeactivate();
+    });
     });
 
-		if(!synths[data.id]){
-		synths[data.id]=prepSynths();
-		}
-		var n = synthmap(data.x,data.y);
-		 _x=n[0];
-		 _y=n[1];
-		if(synths[data.id]){
-		if(synths[data.id][0] && synths[data.id][1]){
-			synths[data.id][0].frequency.value=_x;
-			synths[data.id][1].frequency.value=_y;
 
-			synths[data.id][0].noteOn(0);
-		}
+var touchActivate = function(event){
+  pressed= true;
+  var c = event.gesture.center;
+  var x = c.pageX;
+  var y = c.pageY;
+  $("#press").html("");
+  var xRatio = x/$(window).width();
+  var yRatio = y/$(window).height();
+  var data = {
+    x:xRatio,
+    y:yRatio,
+    col:col,
+    id:client_id,
+  }
+  sendData(data);
+  playSynth(data);
+}
 
-		}
-
-    		});
-
-	function sendMousePosition(mp) {
-		//if mouse is pressed, emit
-		var now = (new Date()).getTime();
-		if(pressed && (now-time>40)){
-			socket.emit('move', {
-			  x: mp.pageX,
-			  y: mp.pageY,
-			  w: $(window).width(),
-			  h: $(window).height()
-			});
-			time= (new Date()).getTime();
-		}
-
-	  return true;
-	}
+var touchDeactivate = function(){
+  pressed=false;
+  socket.emit('silent',{state:"stop"});
+}
 
 
-	function prepSynths(){
-		var oscillator = context.createOscillator(); //need perens here
-		oscillator.type=1;
-		oscillator.frequency.value=_x;
-		//filter
-		var filter = context.createBiquadFilter();
-		filter.type=0;
-		filter.frequency.value=_y;
+//socketio
 
-		//volume
-		var gainNode = context.createGainNode();
-		gainNode.gain.value=0.05;
+var socket = io.connect('http://'+window.location.hostname);
 
-		//connect it all
-		oscillator.connect(filter);
-		filter.connect(gainNode);
-		gainNode.connect(context.destination);
+function synthmap(x,y){
+  tx = 40*Math.pow(2,x*4);
+  ty = 100*Math.pow(2,(1-y)*6);
+  return [tx,ty]
+}
 
-		return [oscillator, filter]
-		}
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+
+socket.on('connect',function(){
+  client_id = socket.socket.sessionid;
+  console.log(client_id);
+
+  if(typeof client_id != "undefined"){
+  $('body').append('<span class="synth" id="synth_'+client_id+'"><span style="display:none;" class="chat"/></span>');
+  }
+
+  $('#synth_'+client_id).css({
+    'left' : ($(window).width()/2-20) + 'px',
+    'top' : $(window).height()/2-20 + 'px',
+    'background-color': col,
+    'opacity' : '0.2'
+  });
+
+  if(typeof client_id !="undefined"){
+    synths[client_id]=prepSynths();
+  }
+
 });
 
+socket.on('silent',function(id){
+  $('#synth_'+id).css({
+    'opacity' : '0.2'
+  });
+
+  console.log(id);
+  if(synths[id][0].playbackState>1){
+    synths[id][0].stop(0);
+  }
+  synths[id]=prepSynths();
+});
+
+socket.on('move', function (data) {
+  playSynth(data);
+});
+
+var playSynth = function(data){
+  if($('#synth_'+data['id']).length == 0 && typeof data['id'] !="undefined") {
+    $('body').append('<span class="synth" id="synth_'+data['id']+'"><span style="display:none;" class="chat"/></span>');
+  }
+
+  $('#synth_'+data['id']).css({
+    'left' : (data['x']*$(window).width()-20) + 'px',
+    'top' : data['y']*$(window).height()-20 + 'px',
+    'background-color': data['col'],
+    'opacity' : '0.7'
+  });
+
+  if(!synths[data.id] && typeof data.id != "undefined"){
+    synths[data.id]=prepSynths();
+  }
+  var n = synthmap(data.x,data.y);
+  _x=n[0];
+  _y=n[1];
+  if(synths[data.id]){
+    if(synths[data.id][0] && synths[data.id][1]){
+      synths[data.id][0].frequency.value=_x;
+      synths[data.id][1].frequency.value=_y;
+      synths[data.id][2].gain.value=(0.2+data.y/3);
+      if(synths[data.id][0].playbackState==0){
+       synths[data.id][0].start(0);
+      }
+    }
+  }
+
+}
+
+socket.on('close', function (id) {
+  console.log('disconnect ' + id);
+  if(id in synths){
+    if(synths[id][0].playbackState>1){
+      synths[id][0].stop(0);
+      synths[id][0].disconnect(0);
+    }
+  }
+  $('#synth_'+id).remove();
+});
+
+
+function sendData(data) {
+  var now = (new Date()).getTime();
+  if(typeof client_id != "undefined" && pressed && (now-time>40)){
+    socket.emit('move', data);
+    time= (new Date()).getTime();
+  }
+  return true;
+}
+
+
+function prepSynths(){
+  var oscillator = context.createOscillator(); //need perens here
+  oscillator.type=1;
+  oscillator.frequency.value=_x;
+  //filter
+  var filter = context.createBiquadFilter();
+  filter.type=0;
+  filter.frequency.value=_y;
+
+  //volume
+  var gainNode = context.createGain();
+  gainNode.gain.value=0.05;
+
+  //connect it all
+  oscillator.connect(filter);
+  filter.connect(gainNode);
+  gainNode.connect(context.destination);
+  return [oscillator, filter, gainNode]
+}
+
+function hsvToRgb(h, s, v){
+  var r, g, b;
+
+  var i = Math.floor(h * 6);
+  var f = h * 6 - i;
+  var p = v * (1 - s);
+  var q = v * (1 - f * s);
+  var t = v * (1 - (1 - f) * s);
+
+  switch(i % 6){
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+}
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = require('http').createServer(handler)
   , fs = require('fs')
 
 app.listen(8000);
+io.set('log level', 1);
 
-colors = {}
 
 function handler (req, res) {
    var filePath = req.url;
@@ -23,55 +23,22 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (client) {
-  if(!colors[client.id]){
-	 col = hsvToRgb(Math.random(),1,1);
-	 colors[client.id]= 'rgb('+col[0]+','+col[1]+','+col[2]+')';
-
-  }
+  console.log(client.id);
 
    client.on('move', function(message) {
-    message.id = client.id;
-	message.col=colors[client.id];
-    console.log(client.id);
-
-    io.sockets.emit('move', message);
-
+    client.broadcast.emit('move', message);
   });
-
 
    client.on('silent',function(){
 	io.sockets.emit('silent',client.id);
    });
 
-
-  client.on('disconnect', function(){
-   	io.sockets.emit('silent', client.id);
-	 io.sockets.emit('close', client.id);
-	 delete colors[client.id];
-  });
-
+   client.on('disconnect', function(){
+     io.sockets.emit('close', client.id);
+   });
 
 });
 
-function hsvToRgb(h, s, v){
-    var r, g, b;
 
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch(i % 6){
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-
-    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
-}
 
 
